1e900b9c6f57a4671dfe4953494c56e511d2b880
VojtechNovak <vojta.novak2005@gmail.com>
2026-06-22 16:20:06 +0200

    feat: Add logic for Frame Buffer and tests for buffer

---
+++ include/FrameBuffer.hpp
@@ -0,0 +1,20 @@
+#include <cstddef>
+#include <cstdint>
+
+
+class FrameBuffer {
+private:
+    uint8_t* m_data;
+    size_t m_w;
+    size_t m_h;
+    unsigned short int m_channels;
+public:
+    FrameBuffer(uint8_t* data, size_t w, size_t h, unsigned short int channels) :
+        m_data(data), m_w(w), m_h(h), m_channels(channels) {};
+    ~FrameBuffer() { delete[] m_data; }
+    uint8_t* getData() { return m_data; };
+    uint8_t* getData() const { return m_data; };
+    size_t getWidth() const { return m_w; }
+    size_t getHeight() const { return m_h; }
+    unsigned short int getChannels() const { return m_channels; }
+};
---
+++ test/test.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <fstream>
+#include "../include/FrameBuffer.hpp"
+
+#define UNIT_TEST(condition, message) \
+    do { \
+        if(!(condition)) { \
+            std::cout << " [FAIL] " << message << "(Row : " << __LINE__ << " )\n"; \
+            return false; \
+        }   \
+    } while(0)
+
+bool testFrameBufferConstructor() {
+    size_t w = 100;
+    size_t h = 50;
+    unsigned short int ch = 3;
+
+    uint8_t* mockData = new uint8_t[w*h*ch];
+    for (size_t i = 0; i < w*h*ch; i += ch) {
+        mockData[i] = 255;
+        mockData[i+1] = 0;
+        mockData[i+2] = 0;
+    }
+    FrameBuffer fb(mockData, w, h, ch);
+
+    UNIT_TEST(fb.getWidth() == w, "Width mismatch");
+    UNIT_TEST(fb.getHeight() == h, "Height mismatch");
+    UNIT_TEST(fb.getChannels() == ch, "Channels mismatch");
+    UNIT_TEST(fb.getData() != nullptr, "Data pointer is null");
+
+    std::ofstream ofs("test_output.ppm");
+    ofs << "P3\n" << fb.getWidth() << " " << fb.getHeight() << "\n255\n";
+    uint8_t* dataPtr = fb.getData();
+    for (size_t i = 0; i < fb.getWidth()*fb.getHeight()*fb.getChannels(); i+=fb.getChannels()) {
+        ofs << static_cast<int>(dataPtr[i]) << " " << static_cast<int>(dataPtr[i+1]) << " " << static_cast<int>(dataPtr[i+2]) << "\n";
+    }
+    ofs.close();
+    std::cout << "Test finished. Check the result...";
+    return true;
+}
+
+void runAllTests() {
+    std::cout << "Running tests!\n";
+
+    int passed = 0;
+    int total = 0;
+    auto runTest = [&passed, &total](bool (*testFunc)(), const std::string& name) {
+        total++;
+        std::cout << "Runs: " << name << " ... ";
+        if (testFunc()) {
+            std::cout << "OK\n";
+            passed++;
+        }
+    };
+    runTest(testFrameBufferConstructor, "Init Frame Buffer");
+    std::cout << "Results: " << passed << " / " << total << "\n";
+}
+
+int main() {
+    runAllTests();
+    return 0;
+}
